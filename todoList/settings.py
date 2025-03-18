f3e58098500c54ec2a182c89787bb5a29fe156ce
aakrit <aakritdongol123@gmail.com>
2025-03-18 16:19:43 +0545

    ip update

--- todoList/settings.py
+++ todoList/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-b#1&hn+e8%3s2j++!^f!ft*8p$4)g@2fz1#uwawk$-^m)&fc2v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
-
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.49.2']
+# http://192.168.49.2:30888 
 
 # Application definition
 
