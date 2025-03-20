9c868853a933e623fb7814a914df5fdf1c7bb09f
aakrit <aakritdongol123@gmail.com>
2025-03-20 08:56:44 +0545

    test

--- todoList/settings.py
+++ todoList/settings.py
@@ -138,7 +138,7 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_wdadwURL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
